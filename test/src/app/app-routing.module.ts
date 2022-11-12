ffd151bce7ce6a56b63905106d6a7ac8089a3dd7
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-12 23:34:30 +0300

    added tabs

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -2,19 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'members',
+  //   loadChildren: () => import('./members/members.module').then(m => m.MembersPageModule)
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
+  // },
+  // {
+  //   path: 'settings',
+  //   loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
+  // },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./tablinks/tablinks.module').then(m => m.TablinksPageModule)
   },
-  {
-    path: 'members',
-    loadChildren: () => import('./members/members.module').then( m => m.MembersPageModule)
-  },
+
 ];
 
 @NgModule({
