dab05eda897139497e5f32edc5c06717fbc8306d
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-13 00:07:30 +0300

    edit tab size

--- test/src/app/tablinks/tablinks-routing.module.ts
+++ test/src/app/tablinks/tablinks-routing.module.ts
@@ -4,42 +4,42 @@ import { Routes, RouterModule } from '@angular/router';
 import { TablinksPage } from './tablinks.page';
 
 const routes: Routes = [
-  {
-    path: 'tablinks',
-    component: TablinksPage,
-    children: [
-      {
-        path: 'members',
-        loadChildren: () => import('../members/members.module').then(m => m.MembersPageModule)
-      },
-      {
-        path: 'profile',
-        loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
-      },
-      {
-        path: 'settings',
-        loadChildren: () => import('../settings/settings.module').then(m => m.SettingsPageModule)
-      },
-      {
-        path: 'home',
-        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
-      },
-      {
+    {
+        path: 'tablinks',
+        component: TablinksPage,
+        children: [
+            {
+                path: 'members',
+                loadChildren: () => import('../members/members.module').then(m => m.MembersPageModule)
+            },
+            {
+                path: 'profile',
+                loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
+            },
+            {
+                path: 'settings',
+                loadChildren: () => import('../settings/settings.module').then(m => m.SettingsPageModule)
+            },
+            {
+                path: 'home',
+                loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+            },
+            {
+                path: '',
+                redirectTo: '/tablinks/profile',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
         path: '',
         redirectTo: '/tablinks/profile',
         pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/tablinks/profile',
-    pathMatch: 'full'
-  }
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class TablinksPageRoutingModule { }
