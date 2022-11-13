d436f03963667bbd73ad825f2294e942662b14a5
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-13 16:45:56 +0300

    added toggle

--- test/src/app/profile/profile.page.ts
+++ test/src/app/profile/profile.page.ts
@@ -1,15 +1,30 @@
+import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+    selector: 'app-profile',
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+    user;
 
-  ngOnInit() {
-  }
+    constructor(public navCtrl: NavController,
+        user: UserService) {
+        console.log(user.name);
+        this.user = user;
+    }
+    ngOnInit() {
+    }
+
+    public data: any = { myToggle: true };
+    isClicked(myToggle) {
+    }
+
+    goToPage2() {
+    }
+
 
 }
