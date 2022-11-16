8ffab2e6fc5fb343a0bcf1d02a1b5963c0629259
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-16 12:47:06 +0300

    item-view

--- mid/mid/src/app/item-view/item-view.page.ts
+++ mid/mid/src/app/item-view/item-view.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService, Product } from '../data.service';
 
 @Component({
   selector: 'app-item-view',
@@ -8,11 +8,18 @@ import { DataService } from '../data.service';
 })
 export class ItemViewPage implements OnInit {
 
-  list: any;
+  list: Product[] = [];
+  currentItem: any;
   constructor(dataSer: DataService) {
     this.list = dataSer.List;
   }
 
+  onSelect(e: any) {
+    console.log(e.target.value);
+    let id: number = e.target.value;
+    this.currentItem = this.list.find(product => product.id == id);
+    console.log(this.currentItem);
+  }
   ngOnInit() {
   }
 
