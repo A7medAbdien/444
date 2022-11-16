a75c406300c94cd1b05de8b24e36847d12d7775e
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-16 11:17:24 +0300

    home

--- mid/mid/src/app/data.service.ts
+++ mid/mid/src/app/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+export interface Product {
+  id: number,
+  name: string,
+  score: string,
+  icon: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  List: Product[] = [];
   constructor() { }
 }
