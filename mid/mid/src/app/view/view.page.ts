106b383923b861bc03e9d78eb9046e296fe88511
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-16 11:59:19 +0300

    view-page

--- mid/mid/src/app/view/view.page.ts
+++ mid/mid/src/app/view/view.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  constructor(public formBuilder: FormBuilder, dataSer: DataService) {
+    this.productForm = formBuilder.group({
+      productId: [''],
+      productName: [''],
+      productScore: [''],
+      productIcon: [''],
+    });
+    this.list = dataSer.List;
+  }
+
+
+  // ------------------------- attributes -------------------------
+  productForm: FormGroup;
+  list;
+
+  // ------------------------- Methods -------------------------
+  insertItem(data: any): void {
+    this.list.push({
+      id: data.productId,
+      name: data.productName,
+      score: data.productScore,
+      icon: data.productIcon,
+    });
+    console.log(this.list);
+  }
 }
