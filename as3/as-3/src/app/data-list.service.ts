30ea75fc8301778af8b844516878d7aedcc0830a
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-01 23:06:03 +0300

    add current student

--- as3/as-3/src/app/data-list.service.ts
+++ as3/as-3/src/app/data-list.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
 
 export interface Student {
   name: string,
-  age: number,
+  age: string,
   gender: string,
-  phone: number,
+  phone: string,
   email: string,
   membership_type: string,
   my_fields: string[]
@@ -19,6 +19,16 @@ export class DataListService {
 
   constructor(public alertCtrl: AlertController, public navCtrl: NavController) { }
 
+  public current_student: Student = {
+    name: '',
+    age: '',
+    gender: '',
+    phone: '',
+    email: '',
+    membership_type: '',
+    my_fields: []
+  };
+
   public name;
   public age;
   public gender;
@@ -31,8 +41,8 @@ export class DataListService {
   public membership_types = ['Weekly', 'Monthly', 'Free Time']
   public membership_type;
 
+  // fields of interest
   public my_fields: string[] = [];
-
   public fields_of_interests = [
     { val: "Watercolor and gouache", checked: false },
     { val: "Sketching and drawing", checked: false },
@@ -42,30 +52,32 @@ export class DataListService {
 
   public list: Student[] =
     [
-      { name: "Ahmed", age: 20, gender: "Male", phone: 33334444, email: "example2.ex", membership_type: "Free Time", my_fields: ["Watercolor and gouache"] },
-      { name: "Ali", age: 22, gender: "Male", phone: 35552223, email: "example3.ex", membership_type: "Weekly", my_fields: ["Mixed media", "Watercolor and gouache"] },
-      { name: "Jojo", age: 24, gender: "Female", phone: 35566223, email: "example1.ex", membership_type: "Monthly", my_fields: ["Mixed media"] }
+      { name: "Ahmed", age: "20", gender: "Male", phone: "33334444", email: "example2.ex", membership_type: "Free Time", my_fields: ["Watercolor and gouache"] },
+      { name: "Ali", age: "22", gender: "Male", phone: "35552223", email: "example3.ex", membership_type: "Weekly", my_fields: ["Mixed media", "Watercolor and gouache"] },
+      { name: "Jojo", age: "24", gender: "Female", phone: "35566223", email: "example1.ex", membership_type: "Monthly", my_fields: ["Mixed media"] }
     ]
 
-  // handing fields
-
+  // ------------ handing alert massage
   private my_fields_list = '';
 
   // to show the fields or field
   private count_fields = 0;
   private show_field = '';
+  // -----------  end of handing alert massage
 
+  // show alert when item clicked, used in tab1 (search member)
   async showAlert(member: Student) {
-    // alert('massage')
 
+    // create field of interest HTML list
     member.my_fields.forEach(f => {
       this.my_fields_list += "<li> " + f + "</li>";
       this.count_fields++;
     });
 
+    // single or pupilar
     (this.count_fields > 1) ? this.show_field = 'Fields' : this.show_field = 'Field';
-    // this.name != null ? this.massage += `Name: ${this.name}<br>` : this.massage += 'Please fill name'
 
+    // alert massage
     let x = await this.alertCtrl.create({
       header: 'Assignment 1',
       subHeader: 'The member of the following information:',
@@ -89,32 +101,28 @@ export class DataListService {
   }
 
   async addWithAlert() {
-    // alert('massage')
-
     this.fields_of_interests.forEach(f => {
       if (f.checked == true) {
         this.my_fields_list += "<li> " + f.val + "</li>";
-        this.my_fields.push(f.val);
+        this.current_student.my_fields.push(f.val);
         this.count_fields++;
       };
     });
-
     (this.count_fields > 1) ? this.show_field = 'Fields' : this.show_field = 'Field';
-    // this.name != null ? this.massage += `Name: ${this.name}<br>` : this.massage += 'Please fill name'
 
     let x = await this.alertCtrl.create({
       header: 'Assignment 1',
       subHeader: 'The member of the following information:',
       message: `
-      Name: ${this.name}<br>
-      Age: ${this.age}<br>
-      Gender: ${this.gender}<br>
+      Name: ${this.current_student.name}<br>
+      Age: ${this.current_student.age}<br>
+      Gender: ${this.current_student.gender}<br>
 
       Contact information: <br>
-      Phone: ${this.phone}<br>
-      Email: ${this.email}<br>
+      Phone: ${this.current_student.phone}<br>
+      Email: ${this.current_student.email}<br>
 
-      Membership Type: ${this.membership_type} <br>
+      Membership Type: ${this.current_student.membership_type} <br>
 
       ${this.show_field} of interest:  ${this.my_fields_list} `,
       buttons: [{
@@ -127,16 +135,16 @@ export class DataListService {
     this.my_fields_list = '';
     this.count_fields = 0;
     x.present();
-    this.list.push(
-      {
-        name: this.name,
-        age: this.age,
-        gender: this.gender,
-        phone: this.phone,
-        email: this.email,
-        membership_type: this.membership_type,
-        my_fields: this.my_fields,
-      })
+    this.list.push(this.current_student)
+    this.current_student = {
+      name: '',
+      age: '',
+      gender: '',
+      phone: '',
+      email: '',
+      membership_type: '',
+      my_fields: []
+    };
   }
 
 }
