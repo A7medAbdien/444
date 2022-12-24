682eb18d15bae297f84113913b7d010afed862cd
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-24 20:07:11 +0300

    edit urls

--- pro/pro/src/app/app.component.ts
+++ pro/pro/src/app/app.component.ts
@@ -14,24 +14,24 @@ export class AppComponent {
           { title: 'Cashier', url: '/cashier-tabs', icon: 'wallet' },
           { title: 'Manage Shifts', url: '/trade-requests', icon: 'calendar' },
           { title: 'Report', url: '/report', icon: 'bar-chart' },
-          { title: 'Suppliers', url: '/slist', icon: 'cube' },
-          { title: 'Employee', url: '/elist', icon: 'briefcase' },
+          { title: 'Suppliers', url: '/sup-list', icon: 'cube' },
+          { title: 'Employee', url: '/emp-list', icon: 'briefcase' },
           { title: 'Orders', url: '/orders-tabs', icon: 'rocket' },
           { title: 'Accounts', url: '/account', icon: 'person' },
         ];
         break;
       case "emp":
         this.appPages = [
-          { title: 'Cashier', url: '/folder/Inbox', icon: 'wallet' },
+          { title: 'Cashier', url: '/cashier-tabs', icon: 'wallet' },
           { title: 'Manage Shifts', url: '/trade-requests', icon: 'calendar' },
-          { title: 'Orders', url: '/folder/Spam', icon: 'rocket' },
-          { title: 'Accounts', url: '/folder/Spam', icon: 'person' },
+          { title: 'Orders', url: '/orders-tabs', icon: 'rocket' },
+          { title: 'Accounts', url: '/account', icon: 'person' },
         ];
         break;
       case "sup":
         this.appPages = [
-          { title: 'Orders', url: '/folder/Spam', icon: 'rocket' },
-          { title: 'Accounts', url: '/folder/Spam', icon: 'person' },
+          { title: 'Orders', url: '/orders-tabs', icon: 'rocket' },
+          { title: 'Accounts', url: '/account', icon: 'person' },
         ];
         break;
       default:
