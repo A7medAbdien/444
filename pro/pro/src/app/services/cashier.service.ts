7e1262366590a3537ce9fdf80e60103fef435011
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-25 00:57:13 +0300

    page 1 all working

--- pro/pro/src/app/services/cashier.service.ts
+++ pro/pro/src/app/services/cashier.service.ts
@@ -15,39 +15,33 @@ export interface Product {
   desertion: string;
 };
 
-export interface CartItem {
-  product: Product;
-  quantity: number;
-}
 export interface Cart {
   id?: string;
-  products: Product[];
-  quantities: number[];
+  cartItems: CartItems,
   total: number;
 }
 
-export interface cartItems { [key: string]: number };
+export interface CartItems { [key: string]: number };
 
 @Injectable({
   providedIn: 'root'
 })
 export class CashierService {
+
+
   public products: Product[] = [
     { id: "123", name: "prod1", quantity: 55, skut: 5, sup: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 12, desertion: "hi" },
     { id: "456", name: "prod2", quantity: 30, skut: 10, sup: "sup1", ipc: 10, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 11, desertion: "hi" },
     { id: "789", name: "prod3", quantity: 45, skut: 5, sup: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 15, desertion: "hi" }
   ];
-  public cart: Cart = {
-    id: "123",
-    products: [{ id: "123", name: "prod1", quantity: 55, skut: 5, sup: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 12, desertion: "hi" }],
-    quantities: [5],
-    total: 5 * 12
-  }
 
-  public cartItem: cartItems = {}
+  public cartItem: CartItems = {}
+  public cart: Cart = { id: "My-cart", total: 0 } as Cart;
+
+  constructor() { }
 
   addToCart(id: any) {
-    if (this.cartItem[id] >= 1) {
+    if (this.cartItem[id] > 0) {
       this.cartItem[id]++;
     } else
       this.cartItem[id] = 1;
@@ -55,19 +49,54 @@ export class CashierService {
   }
 
   removeToCart(id: any) {
-    if (this.cartItem[id] >= 1) {
+    if (this.cartItem[id] > 1) {
       this.cartItem[id]--;
     } else
       delete this.cartItem[id]
     console.log(this.cartItem)
   }
 
-  calcCartTotal(cart: Cart) {
-    for (let i = 0; i < cart.products.length; i++) {
-      const product = cart.products[i];
-      const quantity = cart.quantities[i];
-      cart.total += quantity * product.price;
+  getCartLength() {
+    return Object.keys(this.cartItem).length
+  }
+
+  calcCartTotal() {
+    this.cart.total = 0
+    for (const prod in this.cartItem) {
+      if (Object.prototype.hasOwnProperty.call(this.cartItem, prod)) {
+        const quantity = this.cartItem[prod];
+        const price = this.getProduct(prod)?.price
+        this.cart.total += quantity * price!
+      }
+    }
+    return this.cart.total
+  }
+
+  getProduct(id: string) {
+    for (const i of this.products) {
+      if (i.id == id)
+        return i;
+    }
+    return null;
+  }
+
+  isFiltered: boolean = false;
+  filteredList: Product[] = [] as Product[];
+  filterList(event: any) {
+    if (event.target.value.length > 0) {
+      var filteredlist = this.products.filter((row) => {
+        if (row.name.indexOf(event.target.value) != -1) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      this.filteredList = filteredlist;
+      // console.log(filteredlist)
+      this.isFiltered = true;
+    } else {
+      this.isFiltered = false;
     }
   }
-  constructor() { }
+
 }
