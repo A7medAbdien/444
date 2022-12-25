bc7aeaba7f2b068ca4520e3a5252d1015814ea39
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-25 03:33:43 +0300

    interfaces and edit typo

--- pro/pro/src/app/services/cashier.service.ts
+++ pro/pro/src/app/services/cashier.service.ts
@@ -1,27 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Cart, CartItems, Product } from 'src/interfaces';
 
-export interface Product {
-  id?: string;
-  name: string;
-  quantity: number;
-  // Stock Keeping Unit Threshold
-  skut: number;
-  // TODO: Supplier
-  sup: string;
-  // Item Per Cartoon
-  ipc: number;
-  image: string;
-  price: number;
-  desertion: string;
-};
-
-export interface Cart {
-  id?: string;
-  cartItems: CartItems,
-  total: number;
-}
-
-export interface CartItems { [key: string]: number };
 
 @Injectable({
   providedIn: 'root'
@@ -30,9 +9,9 @@ export class CashierService {
 
 
   public products: Product[] = [
-    { id: "123", name: "prod1", quantity: 55, skut: 5, sup: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 12, desertion: "hi" },
-    { id: "456", name: "prod2", quantity: 30, skut: 10, sup: "sup1", ipc: 10, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 11, desertion: "hi" },
-    { id: "789", name: "prod3", quantity: 45, skut: 5, sup: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 15, desertion: "hi" }
+    { id: "123", name: "prod1", quantity: 55, skut: 5, supId: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 12, description: "hi" },
+    { id: "456", name: "prod2", quantity: 30, skut: 10, supId: "sup1", ipc: 10, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 11, description: "hi" },
+    { id: "789", name: "prod3", quantity: 45, skut: 5, supId: "sup1", ipc: 5, image: "https://ionicframework.com/docs/img/demos/thumbnail.svg", price: 15, description: "hi" }
   ];
 
   public cartItem: CartItems = {}
---
+++ pro/pro/src/interfaces.ts
@@ -0,0 +1,56 @@
+export interface Product {
+  id?: string;
+  name: string;
+  quantity: number;
+  skut: number; // Stock Keeping Unit Threshold
+  supId: string;
+  ipc: number; // Item Per Cartoon
+  image: string;
+  price: number;
+  description: string;
+};
+
+export interface Cart {
+  id?: string;
+  cartItems: CartItems,
+  total: number;
+}
+
+export interface CartItems { [key: string]: number };
+
+export interface Orders extends Product {
+  orderedDate: Date;
+  expectedDate: Date;
+  cartoons: number;
+  orderedBy: string;
+}
+
+export interface User {
+  id?: string;
+  type: string;
+  name: string;
+  email: string;
+  phone: number;
+  image: string;
+}
+
+export interface emp extends User {
+  shifts: Shift[],
+  shiftsRequests: ShiftRequest[]
+}
+
+export interface Shift {
+  id?: string;
+  empId: string;
+  day: Date;
+  startTime: Date;
+  endTime: Date;
+}
+
+export interface ShiftRequest {
+  id?: string;
+  senderEmail: string;
+  receiverEmail: string;
+  shiftId: string;
+  isApproved: boolean;
+}
