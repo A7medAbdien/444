9d2da577b83fe561170c1dee474c54e1f18f2110
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-25 03:34:16 +0300

    page 2 - p1

--- pro/pro/src/app/cashier/tab1/details/details.page.ts
+++ pro/pro/src/app/cashier/tab1/details/details.page.ts
@@ -1,4 +1,6 @@
+import { CashierService } from 'src/app/services/cashier.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-
-  constructor() { }
+  id;
+  p;
+  constructor(public ActRoute: ActivatedRoute, public cashier: CashierService) { }
 
   ngOnInit() {
+    this.id = this.ActRoute.snapshot.paramMap.get('id');
+    this.getProduct(this.id)
   }
+
+  getProduct(id) {
+    this.p = this.cashier.getProduct(id);
+  }
+
 
 }
