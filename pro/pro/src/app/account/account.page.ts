d5cb5ee3c2db19eeb5551a07f4e530ad8a7d6d9d
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-26 18:09:38 +0300

    account done

--- pro/pro/src/app/account/account.page.ts
+++ pro/pro/src/app/account/account.page.ts
@@ -1,3 +1,4 @@
+import { DataService } from 'src/app/services/data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+
+  me
+  constructor(public d: DataService) { }
 
   ngOnInit() {
+    this.me = this.d.me
   }
 
 }
