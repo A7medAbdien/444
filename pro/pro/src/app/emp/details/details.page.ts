13fc37e668dd212c3c255d48365c2730e0efd947
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-26 17:07:47 +0300

    view emp p1

--- pro/pro/src/app/emp/details/details.page.ts
+++ pro/pro/src/app/emp/details/details.page.ts
@@ -1,4 +1,6 @@
+import { DataService } from 'src/app/services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  id
+  emp;
+  constructor(public ActRoute: ActivatedRoute, public d: DataService) { }
 
   ngOnInit() {
+    this.d.onlyOwner()
+    this.id = this.ActRoute.snapshot.paramMap.get('id');
+    this.getSup(this.id)
   }
 
+  getSup(id) {
+    this.emp = this.d.getUser(id)
+  }
+  removeEmp(id) {
+    this.d.removeEmp(id);
+    this.d.removeUser(id);
+  }
 }
