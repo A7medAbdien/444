1d0b6634f4997b69dd3c848effd533ad6b735a13
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-28 18:51:21 +0300

    add -order -p1

--- pro/pro/src/app/orders/tab1/add-order/add-order.page.ts
+++ pro/pro/src/app/orders/tab1/add-order/add-order.page.ts
@@ -12,16 +12,8 @@ export class AddOrderPage implements OnInit {
 
   o
   cartIds
-  addEmp: FormGroup
   constructor(public d: DataService, public ActRoute: ActivatedRoute, public fb: FormBuilder) {
-    this.addEmp = fb.group({
-      name: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z0-9]*'), Validators.minLength(2), Validators.maxLength(30)])],
-      email: ['', Validators.compose([Validators.required, Validators.pattern("[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+")])],
-      pass: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
-      phone: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
-      sup: ['', Validators.compose([Validators.required])],
-      img: [''],
-    });
+
   }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
