5df5abd166abf1b1aad2b66edd240fbb2c98bfb3
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-12-31 14:01:22 +0300

    drag ??

--- test/src/app/home/home.page.ts
+++ test/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
@@ -6,7 +6,7 @@ import { map, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
+import { GestureController, ToastController } from '@ionic/angular';
 
 export interface Idea {
   id?: string,
@@ -14,15 +14,36 @@ export interface Idea {
   notes: string
 }
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor(public toastCtrl: ToastController) { }
+export class HomePage implements AfterViewInit {
+  @ViewChild('rectangle') rect: ElementRef;
+  public type;
+  public currentX;
+  ngAfterViewInit(): void {
+    this.updateGestures();
+  }
+
+  updateGestures() {
+    const drag = this.gestureCtrl.create({
+      el: this.rect.nativeElement,
+      threshold: 1,
+      gestureName: 'drag',
+      onMove: ev => {
+        this.type = ev.type;
+        this.currentX = ev.currentX;
+        if (ev.currentX > 200) this.presentToast()
+        console.log(this.currentX);
+        console.log(this.type);
+      }
+    });
+    drag.enable();
+  }
+
+  constructor(private gestureCtrl: GestureController, public toastCtrl: ToastController) { }
 
   async presentToast() {
     const toast = await this.toastCtrl.create({
