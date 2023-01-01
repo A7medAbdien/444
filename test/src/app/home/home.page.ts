7052f53268a0fd77704df5e7b2240b04321fbd10
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-02 01:33:16 +0300

    login form and FB

--- test/src/app/home/home.page.ts
+++ test/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { DataService } from './../data.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  loginForm: FormGroup;
+
+  constructor(public d: DataService, public formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z@.]*'), Validators.minLength(8), Validators.maxLength(30)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  login(val) {
+    if (this.loginForm.valid) {
+      this.d.email = val.email;
+      this.d.password = val.password;
+      this.d.loginUser(val.email, val.password).then(() => { console.log('Login Successful ' + val.email); }
+      );
+    }
+  }
 }
