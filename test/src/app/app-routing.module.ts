14e2333fa2b4e83ec0741a296ae60ebb86610069
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-01-01 23:43:25 +0300

    base sated

--- test/src/app/app-routing.module.ts
+++ test/src/app/app-routing.module.ts
@@ -3,21 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'cashier-links',
-    loadChildren: () => import('./cashier/cashier-links/cashier-links.module').then(m => m.CashierLinksPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'cashier-links',
+    redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'members',
-    loadChildren: () => import('./members/members.module').then(m => m.MembersPageModule)
   },
 ];
 
